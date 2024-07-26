28faf645e24744c604dcdb250a170e831a4515b3
Memet Emin Öztürk <emin.ozturk@detaysoft.com>
2024-07-26 16:50:26 +0300

    Templates updated with the real values. Localstorage changed with session.

--- src/redux/templates/templateSlicer.js
+++ src/redux/templates/templateSlicer.js
@@ -3,51 +3,82 @@ import toast from 'react-hot-toast';
 
 const templateSlice = createSlice({
     name: 'templates',
-    initialState: JSON.parse(localStorage.getItem('templates')) || [
+    initialState: JSON.parse(sessionStorage.getItem('templates')) || [
         {
             id: 1,
             name: 'KPSS',
-            examTime: 3 * 60 * 60 * 1000,
+            examTime: 180 * 60 * 1000,
             subjects: [
                 { name: "Genel Yetenek", questionCount: 60 },
                 { name: "Genel Kültür", questionCount: 60 },
-                // { name: "Eğitim Bilimleri", questionCount: 30 },
-                // { name: "Alan Bilgisi", questionCount: 30 }
             ],
             questionCount: 120,
             examType: "multiSubject"
         },
         {
             id: 2,
-            name: "ALES",
-            questionCount: 80,
-            examTime: 3 * 60 * 60 * 1000,
-            examType: "singleSubject",
+            name: "TYT",
+            examTime: 165 * 60 * 1000,
+            examType: "multiSubject",
+            subjects: [
+                { id: 1, name: "Türkçe", questionCount: 40 },
+                { id: 2, name: "Sosyal Bilimler", questionCount: 20 },
+                { id: 3, name: "Temel Matematik", questionCount: 40 },
+                { id: 4, name: "Fen Bilimleri", questionCount: 20 }
+            ]
         },
         {
             id: 3,
-            name: "YDS",
-            questionCount: 80,
-            examTime: 2 * 60 * 60 * 1000,
-            examType: "singleSubject"
-        },
-        {
-            id: 4,
-            name: "DGS",
-            questionCount: 80,
-            examTime: 2 * 60 * 60 * 1000,
-            examType: "singleSubject"
-        },
-        {
-            id: 5,
-            name: "YKS",
-            examTime: 3 * 60 * 60 * 1000,
+            name: "AYT",
+            examTime: 180 * 60 * 1000,
             examType: "multiSubject",
             subjects: [
                 { id: 1, name: "Türk Dili ve Edebiyatı", questionCount: 40 },
-                { id: 2, name: "Sosyal Bilimler", questionCount: 40 },
-                { id: 3, name: "Temel Matematik", questionCount: 40 },
+                { id: 2, name: "Sosyal Bilimler-1", questionCount: 40 },
+                { id: 3, name: "Matematik", questionCount: 40 },
                 { id: 4, name: "Fen Bilimleri", questionCount: 40 }
+            ]
+        },
+        {
+            id: 4,
+            name: "LGS - Sözel",
+            examTime: 75 * 60 * 1000,
+            examType: "singleSubject",
+            questionCount: 50
+        },
+        {
+            id: 5,
+            name: "LGS - Sayısal",
+            examTime: 80 * 60 * 1000,
+            examType: "singleSubject",
+            questionCount: 40
+        },
+        {
+            id: 6,
+            name: "YDS",
+            questionCount: 80,
+            examTime: 180 * 60 * 1000,
+            examType: "singleSubject"
+        },
+        {
+            id: 7,
+            name: "ALES",
+            examTime: 150 * 60 * 1000,
+            examType: "multiSubject",
+            subjects: [
+                { id: 1, name: "Sözel", questionCount: 50 },
+                { id: 2, name: "Sayısal", questionCount: 50 }
+            ]
+
+        },
+        {
+            id: 8,
+            name: "DGS",
+            examTime: 135 * 60 * 1000,
+            examType: "multiSubject",
+            subjects: [
+                { id: 1, name: "Sözel", questionCount: 50 },
+                { id: 2, name: "Sayısal", questionCount: 50 }
             ]
         },
     ],
@@ -55,17 +86,17 @@ const templateSlice = createSlice({
         addTemplate: (state, action) => {
             action.payload.id = state.length + 1;
             state.push(action.payload);
-            localStorage.setItem('templates', JSON.stringify(state));
+            sessionStorage.setItem('templates', JSON.stringify(state));
         },
         removeTemplate: (state, action) => {
             const newState = state.filter((template) => template.id !== action.payload);
-            localStorage.setItem('templates', JSON.stringify(newState));
+            sessionStorage.setItem('templates', JSON.stringify(newState));
             return newState;
         },
         updateTemplate: (state, action) => {
             const index = state.findIndex((template) => template.id === action.payload.id);
             state[index] = action.payload;
-            localStorage.setItem('templates', JSON.stringify(state));
+            sessionStorage.setItem('templates', JSON.stringify(state));
         },
     },
 });
