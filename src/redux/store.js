3faf04c3dde9101fcca131930691ffa5a6605b92
Memet Emin Öztürk <emin.ozturk@detaysoft.com>
2025-04-19 12:00:48 +0300

    refactor: moved and renamed redux localstorage helper file

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import opticReducer from './optic/opticSlice';
 import templatesReducer from './templates/templateSlicer';
 import { localStorageMiddleware } from './middleware/localStorageMiddleware';
-import { loadOpticState } from '../helpers/localStorage';
+import { loadOpticState } from './helpers/localStorage';
 
 const preloadedOpticState = loadOpticState();
 
